75129a242bf375da70de734e60e02e3a3cbf4a98
Joseph Shell <josephshell60@gmail.com>
2026-05-14 18:18:37 -0400

    Added skeleton of what a wharf will look like

---
+++ scenes/trade/wharf/wharf.gd
@@ -0,0 +1,23 @@
+class_name Wharf extends Area3D
+
+signal wharf_entered(wharf: Wharf, area: Area3D)
+signal wharf_exited(wharf: Wharf, area: Area3D)
+
+@export var wharf_name: String = "UNNAMED"
+@export var ships_available: Array[String] = []
+
+@onready var billboard: Label3D = %Billboard
+
+var price_provider: PriceProvider
+
+func _ready():
+	area_entered.connect(func(area): 
+		wharf_entered.emit(self, area)
+	)
+	area_exited.connect(func(area):
+		wharf_exited.emit(self, area)
+	)
+	billboard.text = wharf_name
+
+func get_ships_available() -> Array[String]:
+	return ships_available
